d704005364884af11d0180e4d5b82434041e3fc8
Jonathan Prasetya Wijaya <jonathanpwijaya@gmail.com>
2021-09-07 04:05:22 +0700

    Updates

---
+++ _nuxt/static/1630962316/portfolios/sikomo-dashboard/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fjoepw.com\u002F_nuxt\u002Fstatic\u002F1630962316",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolios\u002Fsikomo-dashboard",config:{_app:{basePath:"\u002Fjoepw.com\u002F",assetsPath:"\u002Fjoepw.com\u002F_nuxt\u002F",cdnURL:a}}}}(null));
