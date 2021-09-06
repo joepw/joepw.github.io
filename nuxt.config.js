600b22b4dd33eab4b56d68441570bd2c5849b82d
Jonathan Prasetya Wijaya <jonathanpwijaya@gmail.com>
2021-09-07 02:09:48 +0700

    upgrade dependencies, add CNAME and .nojekyll

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  router: {
+    base: '/joepw.com/'
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Jonathan Prasetya | Web Developer',
