d189911fb521a4c4052704b284e591f53f68d860
Jonathan Prasetya Wijaya <jonathanpwijaya@gmail.com>
2021-09-07 02:43:10 +0700

    Updates

--- _nuxt/static/1630957005/portfolios/sikomo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630957005",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolios\u002Fsikomo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
